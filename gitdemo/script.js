3393e98ccd1435c36f27b09dd412babaf57e3d5f
divikdubey <divik2728@gmail.com>
2026-07-25 16:21:01 +0530

    demo

---
+++ gitdemo/script.js
@@ -0,0 +1,45 @@
+let btn = document.querySelector("button");
+let url2= "https://dog.ceo/api/breeds/image/random";
+
+btn.addEventListener("click", async () =>{
+    let link = await getImage();
+   // console.log(fact);
+    let img = document.querySelector("#result");
+    img.setAttribute("src", link);
+    console.log(link);
+});
+
+
+
+
+async function getImage() {
+    try{
+        let res = await axios.get(url2);
+       return res.data.message;
+    }
+    catch(e){
+        console.log("error-", e);
+        return "NO FACT FOUND";
+    }
+}
+
+// btn.addEventListener("click", async () =>{
+//     let fact = await getCatFact();
+//     console.log(fact);
+//     let p = document.querySelector("#result");
+//     p.innerText = fact;
+// });
+
+// let url = "https://catfact.ninja/fact";
+
+
+// async function getCatFact() {
+//     try{
+//         let response = await axios.get(url);
+//        return response.data.fact;
+//     }
+//     catch(e){
+//         console.log("error-", e);
+//         return "NO FACT FOUND";
+//     }
+// }
